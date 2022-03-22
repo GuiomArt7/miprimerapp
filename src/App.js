5daa10b8d7cdfc7d39fb59bcfab2870d36ce7a25
Guiomar Cameras <ada_hack243@hackademy.mx>
2022-03-21 23:03:48 -0600

    users API

--- src/App.js
+++ src/App.js
@@ -3,45 +3,37 @@ import logo from './logo.svg';
 import {Boton} from "./components/Boton";
 import './App.css';
 
+let urlApi = "https//jsonplaceholder.typicode.com/users";
+
+//Función que renderiza nuestra app
 function App() {
+  //Cargando la petición a la API
   const [loading, setLoading] = useState(false);
-  //* Use state
-  const [contador, setContador] = useState(0);
-  const [nombre, setNombre] = useState("");
-  //*Use Effect
+  //Error
+  const[error, setError]= useState(false);
+  //Data
+  const [data, setData]= useState([]);
+  //Función FETCH: Llama a una API(BACKEND)
+  //FUNCIÓN DE FLECHA: nombreFunción = () => {}
+
+  //Async: función asíncrona
+  //Await: esperar
+  const getUsers = async()=>{
+    try{
+      //Todo el código que puede romperse o mandar a un error
+      let response = await fetch(urlApi)
+    }catch(error){
+      //Es el código que se ejecutará cuando ocurra algún error
+      console.log(error)
+    }
+  };
   useEffect(()=>{
-    setContador(0);
-  }, [nombre]);
+    getUsers();
+  },[])
 
   return (
   <div className="App">
-      <div>
-        El valor de mi contador es: {contador}
-      </div>
-      <button
-      onClick={()=>{
-        setContador(contador + 1);
-      }}>Aumentar contador
-      </button>
-      <button
-      onClick={()=>{
-        setContador(contador - contador);
-      }}>Iniciar cuenta desde cero
-      </button>
-      <button
-      onClick={()=>{
-        setContador(contador - 1);
-      }}>Cuenta regresiva
-      </button>
-      <input
-        value={nombre}
-        onChange={(event) => {
-          setNombre(event.target.value);
-        }}
-        placeholder="Ingresa tu nombre"
-      />
-      <span>Mi nombre es: {nombre}</span>
-      <Boton />
+      <Boton text="Llammar a API"/>
   </div>
   );
 }
